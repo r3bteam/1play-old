314ba0e814fec65fc1bb7af44e0ece047694caa9
r3bteam <39262475+r3bteam@users.noreply.github.com>
2018-12-02 18:38:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -193,10 +193,9 @@ ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 		if (!serverQueue) return msg.channel.send('There is nothing playing.');
 
 		if (!args[1]) return msg.channel.send(`The current volume is: **${serverQueue.volume}**`);
-
+		
 		serverQueue.volume = args[1];
-
-		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 5);
+        serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 50);
 
 		return msg.channel.send(`I set the volume to: **${args[1]}**`);
 
